11b6a86b6fb3e2b06ebcc0b2fc255fe15d4aaef8
Akua66 <Akaaku2021media01@gmail.com>
2024-03-27 17:17:26 +0900

    fix: style

--- src/components/(pages)/loginPage.tsx
+++ src/components/(pages)/loginPage.tsx
@@ -10,7 +10,6 @@ import CustomLink from '../(parts)/CustomButton';
 import { judgeStatus, fetch_doClientCredentials } from '@/libs/APIhandler';
 
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import Modal from '@mui/material/Modal';
@@ -74,7 +73,6 @@ function Login() {
     transform: 'translate(-50%, -50%)',
     width: "70%",
     bgcolor: 'background.paper',
-    border: '2px solid #000',
     boxShadow: 24,
     p: 4,
   };
