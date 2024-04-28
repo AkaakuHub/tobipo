3566c6c1d7e1b375331be43c5400b0646ec22d92
AkaakuHub <akaaku2021media01@gmail.com>
2024-04-28 19:52:52 +0900

    add; 除外リスト

--- src/app/api/getTobipoPlaylist/route.ts
+++ src/app/api/getTobipoPlaylist/route.ts
@@ -47,8 +47,15 @@ const getPlaylist = async (token: string, kind: string) => {
           isUpToDate = true;
         }
         console.log('Using cached tobipo playlist.');
-        const dataFromFB = await db.ref("tobipoPlaylist/idArray").get();
-        const data = dataFromFB.val();
+
+        const idArrayFromFB = await db.ref("tobipoPlaylist/idArray").get();
+        const excludedIdArrayFromFB = await db.ref("tobipoPlaylist/excludedIdArray").get();
+        const idArray = idArrayFromFB.val();
+        const excludedIdArray = excludedIdArrayFromFB.val();
+
+        // ここから、dataを作成, excludedIdArrayを使って、idArrayから除外
+        const data: string[] = idArray.filter((id: string) => !excludedIdArray.includes(id));
+
         return {
           isUpToDate: isUpToDate,
           data: data
@@ -81,7 +88,24 @@ const callAPI = async (token: string) => {
       });
       items = [...items, ...response.data.items];
     }
-    console.log('Updated tobipo playlist.');
+
+    const tobipoExcludePlaylist = process.env.SPOTIFY_TOBIPO_EXCLUDE_PLAYLIST;
+    let response2 = await axios.get(`https://api.spotify.com/v1/playlists/${tobipoExcludePlaylist}/tracks`, {
+      headers: {
+        'Authorization': `Bearer ${token}`,
+        'Accept-Language': 'ja'
+      }
+    });
+    let items2 = response2.data.items;
+    while (response2.data.next) {
+      response2 = await axios.get(response2.data.next, {
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Accept-Language': 'ja'
+        }
+      });
+      items2 = [...items2, ...response2.data.items];
+    }
 
     // そのままだと不要な情報が多いので、あらかじめ必要な情報だけを抽出
     const extractedItems = extractTobipoData(items, "playlist");
@@ -95,6 +119,14 @@ const callAPI = async (token: string) => {
      * {"12345": {...}, "67890": {...}, ...}
      */
     // return extractedItems;
+
+    // 除外リストを作成
+    const extractedItems2 = extractTobipoData(items2, "playlist");
+    const idArray2 = Object.keys(extractedItems2);
+    // 除外idArrayをFirebaseに保存
+    db.ref("tobipoPlaylist/excludedIdArray").set(idArray2);
+
+    console.log('Updated tobipo playlist.');
     return idArray;
   } catch (error: any) {
     if (error.response && error.response.status === 401) {
